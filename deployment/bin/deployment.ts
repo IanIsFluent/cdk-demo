2155638c462b0668b585c28962cc3bc231477f71
Ian Grainger <ian@isfluent.com>
2022-04-29 10:59:50 +0100

    delete my badly named folder

--- deployment/bin/deployment.ts
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env node
-import * as cdk from 'aws-cdk-lib';
-import { DeploymentStack } from '../lib/deployment-stack';
-
-const app = new cdk.App();
-new DeploymentStack(app, 'DeploymentStack');
