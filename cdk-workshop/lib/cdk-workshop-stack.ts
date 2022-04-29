9b9a01d6c204ce9256006a6a1c608e7090663b10
Ian Grainger <ian@isfluent.com>
2022-04-29 13:44:56 +0100

    add a lambda infront of the other lambda which logs to a dynamodb before invoking it. note grants!

--- cdk-workshop/lib/cdk-workshop-stack.ts
+++ cdk-workshop/lib/cdk-workshop-stack.ts
@@ -2,6 +2,7 @@ import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
 import * as apigw from 'aws-cdk-lib/aws-apigateway';
+import { HitCounter } from './hitcounter';
 
 export class CdkWorkshopStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
@@ -16,9 +17,13 @@ export class CdkWorkshopStack extends Stack {
       handler: 'hello.handler', // entrypoint
     });
 
+    const helloWithCounter = new HitCounter(this, 'HelloHitCounter', {
+      downstream: hello,
+    });
+
     // defines a Lambda Rest Api Gateway in front of the Lambda resource defined above
     new apigw.LambdaRestApi(this, 'Endpoint', {
-      handler: hello,
+      handler: helloWithCounter.handler,
     });
   }
 }
---
+++ cdk-workshop/lib/hitcounter.ts
@@ -0,0 +1,33 @@
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import { Construct } from 'constructs';
+
+export interface HitCounterProps {
+  /** the function for which we want to count url hits **/
+  downstream: lambda.IFunction;
+}
+
+export class HitCounter extends Construct {
+  public readonly handler: lambda.Function;
+
+  constructor(scope: Construct, id: string, props: HitCounterProps) {
+    super(scope, id);
+
+    const table = new dynamodb.Table(this, 'Hits', {
+      partitionKey: { name: 'path', type: dynamodb.AttributeType.STRING },
+    });
+
+    this.handler = new lambda.Function(this, 'HitCounterHandler', {
+      runtime: lambda.Runtime.NODEJS_14_X,
+      handler: 'hitcounter.handler',
+      code: lambda.Code.fromAsset('lambda'), //using the same folder as the other lambda - but calling the other handler
+      environment: {
+        DOWNSTREAM_FUNCTION_NAME: props.downstream.functionName,
+        HITS_TABLE_NAME: table.tableName,
+      },
+    });
+
+    table.grantWriteData(this.handler);
+    props.downstream.grantInvoke(this.handler);
+  }
+}
