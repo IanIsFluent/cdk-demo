7bd59c374bec5c39d1c3ab785655b65476787003
Ian Grainger <ian@isfluent.com>
2022-04-29 16:14:13 +0100

    be more clever with stack names

--- cdk-workshop/bin/cdk-workshop.ts
+++ cdk-workshop/bin/cdk-workshop.ts
@@ -4,7 +4,14 @@ import { CdkWorkshopStack } from '../lib/cdk-workshop-stack';
 
 const app = new cdk.App();
 
-new CdkWorkshopStack(
-  app,
-  `CdkWorkshopStack-${process.env.deployment_environment}`
-);
+new CdkWorkshopStack(app, `CdkWorkshopStack${getEnvName()}`);
+
+function getEnvName() {
+  const fromEnv = process.env.deployment_environment;
+  if (fromEnv === 'main') {
+    return '';
+  } else if (fromEnv) {
+    return `-${fromEnv}`;
+  }
+  return '-dev';
+}
