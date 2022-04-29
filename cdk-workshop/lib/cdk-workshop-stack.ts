13fadb5a7282925ea3135a3c8f13fce5d31cfbe5
Ian Grainger <ian@isfluent.com>
2022-04-29 11:09:22 +0100

    add the hello lambda to the CDK stack

--- cdk-workshop/lib/cdk-workshop-stack.ts
+++ cdk-workshop/lib/cdk-workshop-stack.ts
@@ -1,19 +1,18 @@
 import { Duration, Stack, StackProps } from 'aws-cdk-lib';
-import * as sns from 'aws-cdk-lib/aws-sns';
-import * as subs from 'aws-cdk-lib/aws-sns-subscriptions';
-import * as sqs from 'aws-cdk-lib/aws-sqs';
 import { Construct } from 'constructs';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
 
 export class CdkWorkshopStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
-
-    const queue = new sqs.Queue(this, 'CdkWorkshopQueue', {
-      visibilityTimeout: Duration.seconds(300)
-    });
-
-    const topic = new sns.Topic(this, 'CdkWorkshopTopic');
-
-    topic.addSubscription(new subs.SqsSubscription(queue));
   }
+
+  // defines an AWS Lambda resource
+  // first parameter - scope: almost always this (create child in the context of this stack)
+  // second parameter - id: unique (in the stack) local identity of the construct
+  hello = new lambda.Function(this, 'HelloHandler', {
+    runtime: lambda.Runtime.NODEJS_14_X,
+    code: lambda.Code.fromAsset('lambda'), // this is a path!
+    handler: 'hello.handler', // entrypoint
+  });
 }
